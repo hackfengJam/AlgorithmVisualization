bae4c292376cd2b713bd7779cf25854f50cf813f
HackFun <hackfeng624@gmail.com>
2019-03-24 00:04:37 +0800

    初步完成 插入排序可视化

---
+++ InsertionSortVisualization/src/AlgoFrame.java
@@ -0,0 +1,91 @@
+import com.sun.org.apache.bcel.internal.generic.ALOAD;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AlgoFrame extends JFrame {
+
+    private int canvasWidth;
+    private int canvasHeight;
+
+    public AlgoFrame(String title, int canvasWidth, int canvasHeight) {
+
+        super(title);
+
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+
+        AlgoCanvas canvas = new AlgoCanvas();
+        setContentPane(canvas);
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        setVisible(true);
+    }
+
+    public AlgoFrame(String title) {
+
+        this(title, 1024, 768);
+    }
+
+    public int getCanvasWidth() {
+        return canvasWidth;
+    }
+
+    public int getCanvasHeight() {
+        return canvasHeight;
+    }
+
+    // TODO: 设置自己的数据
+    private InsertionSortData data;
+
+    public void render(InsertionSortData data) {
+        this.data = data;
+        repaint();
+    }
+
+    private class AlgoCanvas extends JPanel {
+
+        public AlgoCanvas() {
+            // 双缓存
+            super(true);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D) g;
+
+            // 抗锯齿
+            RenderingHints hints = new RenderingHints(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.addRenderingHints(hints);
+
+            // 具体绘制
+            int w = canvasWidth / data.N();
+            for (int i = 0; i < data.N(); i++) {
+                if (i < data.orderedIndex)
+                    AlgoVisHelper.setColor(g2d, AlgoVisHelper.Red);
+                else
+                    AlgoVisHelper.setColor(g2d, AlgoVisHelper.Grey);
+
+                if (i == data.currentIndex)
+                    AlgoVisHelper.setColor(g2d, AlgoVisHelper.LightBlue);
+
+                AlgoVisHelper.fillRectangle(g2d, i * w, canvasHeight - data.get(i), w, data.get(i));
+            }
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(canvasWidth, canvasHeight);
+        }
+    }
+}
+
+
---
+++ InsertionSortVisualization/src/AlgoVisualizer.java
@@ -0,0 +1,59 @@
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.MouseAdapter;
+
+public class AlgoVisualizer {
+
+    private static int DELAY = 10;
+    private InsertionSortData data;        // 数据
+    private AlgoFrame frame;    // 视图
+
+    public AlgoVisualizer(int sceneWidth, int sceneHeight, int N) {
+
+        // 初始化数据
+        data = new InsertionSortData(N, sceneHeight);
+
+        // 初始化视图
+        EventQueue.invokeLater(() -> {
+            frame = new AlgoFrame("Welcome", sceneWidth, sceneHeight);
+            new Thread(() -> {
+                run();
+            }).start();
+        });
+    }
+
+    // 动画逻辑
+    private void run() {
+        setData(0, -1);
+
+        for (int i = 0; i < data.N(); i++) {
+            setData(i, i);
+            for (int j = i; j > 0 && data.get(j) < data.get(j - 1); j--) {
+                data.swap(j, j - 1);
+                setData(i + 1, j - 1);
+            }
+        }
+
+        setData(data.N(), -1);
+    }
+
+
+    private void setData(int orderedIndex, int currentIndex) {
+        data.orderedIndex = orderedIndex;
+        data.currentIndex = currentIndex;
+
+        frame.render(data);
+        AlgoVisHelper.pause(DELAY);
+    }
+
+    public static void main(String[] args) {
+
+        int sceneWidth = 800;
+        int sceneHeight = 800;
+
+        int N = 100;
+
+        // TODO: 根据需要设置其他参数，初始化visualizer
+        AlgoVisualizer visualizer = new AlgoVisualizer(sceneWidth, sceneHeight, N);
+    }
+}
